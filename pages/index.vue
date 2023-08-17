5d11628fdc9147d1396a5f4bb413b827c8cdc22d
Shuichi yukimoto <yukimoto@usa-mimi.jp>
2023-08-17 18:45:41 +0900

    chatGTPに対応

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,48 @@
-<template>
+<!-- <template>
     <div class="text-center">
         Hello World
     </div>
 </template>
 
 <script setup>
-definePageMeta({
-    middleware: ['auth']
-})
+// definePageMeta({
+//     middleware: ['auth']
+// })
 function test() {
     console.log('Hello World')
 }
+</script> -->
+
+<script setup>
+const keyword = ref('');
+const generateText = ref('')
+
+const prompt = computed(() => `
+  日本語で回答して下さい。${keyword.value}について説明してください。
+`)
+
+const handleClick = async () => {
+    const { data } = await useFetch('/api/generate', {
+        method: 'POST',
+        body: {
+            prompt
+        }
+    })
+
+    generateText.value = data.value.choices[0].text
+}
 </script>
+
+<template>
+    <h1>GPT-3 APP</h1>
+    <div>
+        <input type="text" v-model="keyword">
+    </div>
+    <div>
+        <button type="button" @click="handleClick">テキスト生成</button>
+    </div>
+    <div>
+        <h2>生成テキスト</h2>
+        <div> {{ generateText }}</div>
+    </div>
+</template>
